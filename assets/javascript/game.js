21e1b3484b80e716c39d17deac2962fb3b9d8aa8
Melvin Hernandez <melher39@gmail.com>
2018-10-22 16:47:36 -0500

    added variables to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
     //code will go here
+    // declare variables
+    // random number that will range from 19-120
+    var randomNumber = "";
+    // point value of the red crystal. will be random
+    var redCrystalValue = "";
+    // point value of the blue crystal. will be random
+    var blueCrystalValue = "";
+    // point value of the yellow crystal. will be random
+    var YellowCrystalValue = "";
+    // point value of the green crystal. will be random
+    var GreenCrystalValue = "";
+    // player's score as they play the game. they need to match this to the randomNumber to win
+    var totalScore = 0;
+
+    var crystalCollectorGame = {
+    // total amount of player wins
+    wins: 0,
+    // total amount of player losses
+    losses: 0
+
+    }
+
+    console.log(randomNumber);
     
 })
