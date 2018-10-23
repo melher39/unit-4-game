ace9333be0e4c16c90ff8d6abca31cbfa0209a1c
Melvin Hernandez <melher39@gmail.com>
2018-10-22 20:08:56 -0500

    random numbers generated. added correctly when clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,70 @@ $(document).ready(function(){
     // declare variables
     // random number that will range from 19-120
     var randomNumber = "";
+    // total amount of player wins
+    var wins = 0;
+    // total amount of player losses
+    var losses = 0;
     // point value of the red crystal. will be random
-    var redCrystalValue = "";
+    var redCrystalValue= "";
     // point value of the blue crystal. will be random
     var blueCrystalValue = "";
     // point value of the yellow crystal. will be random
-    var YellowCrystalValue = "";
+    var yellowCrystalValue = "";
     // point value of the green crystal. will be random
-    var GreenCrystalValue = "";
+    var greenCrystalValue = "";
     // player's score as they play the game. they need to match this to the randomNumber to win
     var totalScore = 0;
 
-    var crystalCollectorGame = {
-    // total amount of player wins
-    wins: 0,
-    // total amount of player losses
-    losses: 0
-
+    function setupGame() {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        redCrystalValue = Math.floor(Math.random() * 12) + 1;
+        blueCrystalValue = Math.floor(Math.random() * 12) + 1;
+        yellowCrystalValue = Math.floor(Math.random() * 12) + 1;
+        greenCrystalValue = Math.floor(Math.random() * 12) + 1;
+        $("#random-number").text(randomNumber);
     }
 
+    setupGame();
+
+
+    $("#red-crystal").on("click", function() {
+        // alert("i've been clicked");
+        totalScore = redCrystalValue + totalScore;
+        $("#total-score").text(totalScore);
+        console.log(totalScore);
+    });
+
+    $("#blue-crystal").on("click", function() {
+        // alert("i've been clicked");
+        totalScore = blueCrystalValue + totalScore;
+        $("#total-score").text(totalScore);
+        console.log(totalScore);
+    });
+
+
+    $("#yellow-crystal").on("click", function() {
+        // alert("i've been clicked");
+        totalScore = yellowCrystalValue + totalScore;
+        $("#total-score").text(totalScore);
+        console.log(totalScore);
+    });
+
+    $("#green-crystal").on("click", function() {
+        // alert("i've been clicked");
+        totalScore = greenCrystalValue + totalScore;
+        $("#total-score").text(totalScore);
+        console.log(totalScore);
+    });
+
+    // setupGame();
     console.log(randomNumber);
+    console.log(redCrystalValue);
+    console.log(blueCrystalValue);
+    console.log(yellowCrystalValue);
+    console.log(greenCrystalValue);
+    // console.log(totalScore);
+
     
-})
+    
+});
